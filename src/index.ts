5576075623f3477d800121f3decdbf0b2ef77499
Harsh <harshpareek91@gmail.com>
2024-12-21 11:08:45 +0530

    fix: wrong types for result

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from './aspi';
 export * from './error';
 export * from './http';
 export * from './types';
-export * from './result';
+export * as Result from './result';
 export * from './request';
